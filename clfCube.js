faad4b97cfcd508ef50604c0a66eaeaa4f17a8f8
Zurihaa <117794461+Zurihaa@users.noreply.github.com>
2023-10-16 05:50:18 -0500

    Update

--- clfCube.js
+++ clfCube.js
@@ -8,83 +8,84 @@ var thetaLoc;
 var speed = 100;
 var direction = true;
 
-window.onload = function init()
-{
-    var canvas = document.getElementById( "gl-canvas" );
+var vertices = [
+    vec4(-0.5, -0.5, -0.5, 1.0),
+    vec4(-0.5, 0.5, -0.5, 1.0),
+    vec4(0.5, 0.5, -0.5, 1.0),
+    vec4(0.5, -0.5, -0.5, 1.0),
+    vec4(-0.5, -0.5, 0.5, 1.0),
+    vec4(-0.5, 0.5, 0.5, 1.0),
+    vec4(0.5, 0.5, 0.5, 1.0),
+    vec4(0.5, -0.5, 0.5, 1.0)
+];
 
-    gl = WebGLUtils.setupWebGL( canvas );
-    if ( !gl ) { alert( "WebGL isn't available" ); }
+var colors = [
+    vec4(0.0, 0.0, 0.0, 1.0),   // Black
+    vec4(1.0, 0.0, 0.0, 1.0),   // Red
+    vec4(1.0, 1.0, 0.0, 1.0),   // Yellow
+    vec4(0.0, 1.0, 0.0, 1.0),   // Green
+    vec4(0.0, 0.0, 1.0, 1.0),   // Blue
+    vec4(1.0, 0.0, 1.0, 1.0),   // Magenta
+    vec4(1.0, 1.0, 1.0, 1.0),   // White
+    vec4(0.0, 1.0, 1.0, 1.0)    // Cyan
+];
+
+var cubeVertices = [
+    1, 0, 3, 3, 2, 1,
+    2, 3, 7, 7, 6, 2,
+    3, 0, 4, 4, 7, 3,
+    6, 5, 1, 1, 2, 6,
+    4, 5, 6, 6, 7, 4,
+    5, 4, 0, 0, 1, 5
+];
+
+window.onload = function init() {
+    var canvas = document.getElementById("gl-canvas");
+
+    gl = WebGLUtils.setupWebGL(canvas);
+    if (!gl) {
+        alert("WebGL isn't available");
+    }
 
     //
     //  Configure WebGL
     //
-    gl.viewport( 0, 0, canvas.width, canvas.height );
-    gl.clearColor( 1.0, 1.0, 1.0, 1.0 );
+    gl.viewport(0, 0, canvas.width, canvas.height);
+    gl.clearColor(1.0, 1.0, 1.0, 1.0);
 
-    // Load shaders and initialize attribute buffers
-    var program = initShaders( gl, "vertex-shader", "fragment-shader" );
-    gl.useProgram( program );
+    //  Load shaders and initialize attribute buffers
 
-    // Define cube vertices
-    var vertices = [
-        // Front face
-        vec3(-1, -1, 1), 
-        vec3(1, -1, 1),  
-        vec3(1, 1, 1),   
-        vec3(-1, 1, 1),
-        // Back face
-        vec3(-1, -1, -1), 
-        vec3(1, -1, -1),  
-        vec3(1, 1, -1),   
-        vec3(-1, 1, -1)   
-    ];
-
-    // Define cube colors with opacity
-    var colors = [
-        vec4(0.0, 0.0, 0.0, 1.0),   
-        vec4(1.0, 0.0, 0.0, 1.0),   
-        vec4(1.0, 1.0, 0.0, 1.0),   
-        vec4(0.0, 1.0, 0.0, 1.0),   
-        vec4(0.0, 0.0, 1.0, 1.0),   
-        vec4(1.0, 0.0, 1.0, 1.0),   
-        vec4(1.0, 1.0, 1.0, 1.0),   
-        vec4(0.0, 1.0, 1.0, 1.0)    
-    ];
+    var program = initShaders(gl, "vertex-shader", "fragment-shader");
+    gl.useProgram(program);
 
     // Load the data into the GPU
-
     var bufferId = gl.createBuffer();
     gl.bindBuffer(gl.ARRAY_BUFFER, bufferId);
     gl.bufferData(gl.ARRAY_BUFFER, flatten(vertices), gl.STATIC_DRAW);
 
-    // Associate out shader variables with our data buffer
-
-    var vPosition = gl.getAttribLocation( program, "vPosition" );
-    gl.vertexAttribPointer( vPosition, 3, gl.FLOAT, false, 0, 0 );
+    var vPosition = gl.getAttribLocation(program, "vPosition");
+    gl.vertexAttribPointer(vPosition, 4, gl.FLOAT, false, 0, 0);
     gl.enableVertexAttribArray(vPosition);
 
-    // Define a buffer for colors
     var cBuffer = gl.createBuffer();
     gl.bindBuffer(gl.ARRAY_BUFFER, cBuffer);
     gl.bufferData(gl.ARRAY_BUFFER, flatten(colors), gl.STATIC_DRAW);
 
-    var vColor = gl.getAttribLocation( program, "vColor" );
-    gl.vertexAttribPointer( vColor, 4, gl.FLOAT, false, 0, 0 );
+    var vColor = gl.getAttribLocation(program, "vColor");
+    gl.vertexAttribPointer(vColor, 4, gl.FLOAT, false, 0, 0);
     gl.enableVertexAttribArray(vColor);
 
     thetaLoc = gl.getUniformLocation(program, "theta");
 
     // Initialize event handlers
-
-    //slider
     document.getElementById("slider").onchange = function(event) {
         speed = 100 - event.target.value;
     };
-    //button
+
     document.getElementById("Direction").onclick = function (event) {
         direction = !direction;
     };
-    //menu
+
     document.getElementById("Controls").onclick = function( event) {
         switch(event.target.index) {
             case 0:
@@ -100,7 +101,7 @@ window.onload = function init()
                 break;
         }
     };
-    //keyboard
+
     window.onkeydown = function( event ) {
         var key = String.fromCharCode(event.keyCode);
         switch( key ) {
@@ -116,29 +117,22 @@ window.onload = function init()
                 speed *= 2.0;
                 break;
         }
-    }; 
+    };
 
     render();
 };
 
-function render()
-{
-    gl.clear( gl.COLOR_BUFFER_BIT );
+function render() {
+    gl.clear(gl.COLOR_BUFFER_BIT);
 
-    theta += (direction ? 0.1 : -0.1);
+    theta += direction ? 0.1 : -0.1;
     gl.uniform1f(thetaLoc, theta);
 
-    // Apply a rotation transformation along the Z-axis
-    var rotationMatrix = mat4();
-    rotationMatrix = mult(rotationMatrix, rotate(theta, [0, 0, 1])); // Rotate along Z-axis
+    for (var i = 0; i < 12; i++) {
+        gl.drawArrays(gl.TRIANGLES, i * 6, 6);
+    }
 
-    // Pass the rotation matrix to the shader
-    gl.uniformMatrix4fv(rotateLoc, false, flatten(rotationMatrix));
-
-    gl.drawArrays(gl.TRIANGLES, 0, 36); // Draw the cube
-
-    setTimeout(
-        function () {requestAnimFrame( render );},
-        speed
-    );
+    setTimeout(function () {
+        requestAnimFrame(render);
+    }, speed);
 }
