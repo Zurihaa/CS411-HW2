5437ec82af0007c05e6b59f92fc5e3ebc555f058
Zurihaa <117794461+Zurihaa@users.noreply.github.com>
2023-10-16 22:34:21 -0500

    Finished HW2

--- clfCube.js
+++ clfCube.js
@@ -1,129 +1,104 @@
 "use strict";
 
-// WebGL variables
-let canvas;
-let gl;
+var canvas;
+var gl;
 
-// Cube properties
 var points = [];
 var colors = [];
 
-// Rotation axes and angles
-const xAxis = 0;
-const yAxis = 1;
-const zAxis = 2;
-let currentAxis = xAxis;
-const rotationAngles = [0, 0, 0];
+var NumVertices  = 36;
 
-let thetaLoc;
+window.onload = function init()
+{
+    canvas = document.getElementById( "gl-canvas" );
 
-window.onload = function init() {
-    canvas = document.getElementById("gl-canvas");
+    gl = WebGLUtils.setupWebGL( canvas );
+    if ( !gl ) { alert( "WebGL isn't available" ); }
 
-    gl = WebGLUtils.setupWebGL(canvas);
-    if (!gl) {
-        alert("WebGL isn't available");
-    }
+    colorCube(); // REFERENCED FROM: https://www.cs.unm.edu/~angel/BOOK/INTERACTIVE_COMPUTER_GRAPHICS/SEVENTH_EDITION/CODE/04/cube.html
 
-    createColorCube(); // Reference from https://www.cs.unm.edu/~angel/BOOK/INTERACTIVE_COMPUTER_GRAPHICS/SEVENTH_EDITION/CODE/04/cube.html
     //
-    // Configure WebGL
+    //  Configure WebGL
     //
-    gl.viewport(0, 0, canvas.width, canvas.height);
-    gl.clearColor(1.0, 1.0, 1.0, 1.0);
+    gl.viewport( 0, 0, canvas.width, canvas.height );
+    gl.clearColor( 1.0, 1.0, 1.0, 1.0 );
+
+    // enable hidden-surface removal
+
     gl.enable(gl.DEPTH_TEST);
 
-    // Load shaders and initialize attribute buffers
-    const program = initShaders(gl, "vertex-shader", "fragment-shader");
-    gl.useProgram(program);
+    //  Load shaders and initialize attribute buffers
 
-    // Load the data into the CPU
-    var vBuffer = gl.createBuffer();
-    gl.bindBuffer(gl.ARRAY_BUFFER, vBuffer);
-    gl.bufferData(gl.ARRAY_BUFFER, flatten(points), gl.STATIC_DRAW);
+    var program = initShaders( gl, "vertex-shader", "fragment-shader" );
+    gl.useProgram( program );
 
-    // Associate out shader variables with our data buffer
-    var vPosition = gl.getAttribLocation(program, "vPosition");
-    gl.vertexAttribPointer(vPosition, 4, gl.FLOAT, false, 0, 0);
-    gl.enableVertexAttribArray(vPosition);
+    // Create a buffer object, initialize it, and associate it with the
+    //  associated attribute variable in our vertex shader
 
     var cBuffer = gl.createBuffer();
-    gl.bindBuffer(gl.ARRAY_BUFFER, cBuffer);
-    gl.bufferData(gl.ARRAY_BUFFER, flatten(colors), gl.STATIC_DRAW);
+    gl.bindBuffer( gl.ARRAY_BUFFER, cBuffer );
+    gl.bufferData( gl.ARRAY_BUFFER, flatten(colors), gl.STATIC_DRAW );
 
-    var vColor = gl.getAttribLocation(program, "vColor");
-    gl.vertexAttribPointer(vColor, 4, gl.FLOAT, false, 0, 0);
-    gl.enableVertexAttribArray(vColor);
+    var vColor = gl.getAttribLocation( program, "vColor" );
+    gl.vertexAttribPointer( vColor, 4, gl.FLOAT, false, 0, 0 );
+    gl.enableVertexAttribArray( vColor );
 
-    thetaLoc = gl.getUniformLocation(program, "theta");
+    var vBuffer = gl.createBuffer();
+    gl.bindBuffer( gl.ARRAY_BUFFER, vBuffer );
+    gl.bufferData( gl.ARRAY_BUFFER, flatten(points), gl.STATIC_DRAW );
 
-    // Initailize event handler
-
-    // X axis
-    document.getElementById("xButton").onclick = function () {
-        currentAxis = xAxis;
-    };
-
-    // Y Axis
-    document.getElementById("yButton").onclick = function () {
-        currentAxis = yAxis;
-    };
-
-    // Z Axis
-    document.getElementById("zButton").onclick = function () {
-        currentAxis = zAxis;
-    };
+    var vPosition = gl.getAttribLocation( program, "vPosition" );
+    gl.vertexAttribPointer( vPosition, 4, gl.FLOAT, false, 0, 0 );
+    gl.enableVertexAttribArray( vPosition );
 
     render();
 };
 
-function createColorCube() {
-    quad(1, 0, 3, 2);
-    quad(2, 3, 7, 6);
-    quad(3, 0, 4, 7);
-    quad(6, 5, 1, 2);
-    quad(4, 5, 6, 7);
-    quad(5, 4, 0, 1);
+function colorCube()
+{
+    quad( 1, 0, 3, 2 );
+    quad( 2, 3, 7, 6 );
+    quad( 3, 0, 4, 7 );
+    quad( 6, 5, 1, 2 );
+    quad( 4, 5, 6, 7 );
+    quad( 5, 4, 0, 1 );
 }
 
-function quad(a, b, c, d) {
+function quad(a, b, c, d)
+{
     var vertices = [
-        vec4(-0.5, -0.5, 0.5, 1.0),
-        vec4(-0.5, 0.5, 0.5, 1.0),
-        vec4(0.5, 0.5, 0.5, 1.0),
-        vec4(0.5, -0.5, 0.5, 1.0),
-        vec4(-0.5, -0.5, -0.5, 1.0),
-        vec4(-0.5, 0.5, -0.5, 1.0),
-        vec4(0.5, 0.5, -0.5, 1.0),
-        vec4(0.5, -0.5, -0.5, 1.0)
+        vec4( -0.5, -0.5,  0.5, 1.0 ),
+        vec4( -0.5,  0.5,  0.5, 1.0 ),
+        vec4(  0.5,  0.5,  0.5, 1.0 ),
+        vec4(  0.5, -0.5,  0.5, 1.0 ),
+        vec4( -0.5, -0.5, -0.5, 1.0 ),
+        vec4( -0.5,  0.5, -0.5, 1.0 ),
+        vec4(  0.5,  0.5, -0.5, 1.0 ),
+        vec4(  0.5, -0.5, -0.5, 1.0 )
     ];
 
     var vertexColors = [
-        [0.0, 0.0, 0.0, 1.0], // Black
-        [1.0, 0.0, 0.0, 1.0], // Red
-        [1.0, 1.0, 0.0, 1.0], // Yellow
-        [0.0, 1.0, 0.0, 1.0], // Green
-        [0.0, 0.0, 1.0, 1.0], // Blue
-        [1.0, 0.0, 1.0, 1.0], // Magenta
-        [1.0, 1.0, 1.0, 1.0], // White
-        [0.0, 1.0, 1.0, 1.0]  // Cyan
+        [ 0.0, 0.0, 0.0, 1.0 ],  // black
+        [ 1.0, 0.0, 0.0, 1.0 ],  // red
+        [ 1.0, 1.0, 0.0, 1.0 ],  // yellow
+        [ 0.0, 1.0, 0.0, 1.0 ],  // green
+        [ 0.0, 0.0, 1.0, 1.0 ],  // blue
+        [ 1.0, 0.0, 1.0, 1.0 ],  // magenta
+        [ 0.0, 1.0, 1.0, 1.0 ],  // cyan
+        [ 1.0, 1.0, 1.0, 1.0 ]   // white
     ];
 
-    var indices = [a, b, c, a, c, d];
+    var indices = [ a, b, c, a, c, d ];
 
-    for (let i = 0; i < indices.length; ++i) {
-        points.push(vertices[indices[i]]);
+    for ( var i = 0; i < indices.length; ++i ) {
+        points.push( vertices[indices[i]] );
         colors.push(vertexColors[a]);
     }
 }
 
-function render() {
-    gl.clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT);
 
-    rotationAngles[currentAxis] += 2.0;
-    gl.uniform3fv(thetaLoc, rotationAngles);
-
-    gl.drawArrays(gl.TRIANGLES, 0, 36);
-
-    requestAnimFrame(render);
+function render()
+{
+    gl.clear( gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT);
+    gl.drawArrays( gl.TRIANGLES, 0, NumVertices );
 }
